d9f01f2e920df6fdeff2073d48ef2daf35c72918
tqz <317291862@qq.com>
2020-07-03 23:56:46 +0800

    --

--- src/main/java/com/tlFrameWork/TlFrameWorkApplication.java
+++ src/main/java/com/tlFrameWork/TlFrameWorkApplication.java
@@ -1,4 +1,4 @@
-package com.tlFrameWork.tlFrameWork;
+package com.tlFrameWork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
